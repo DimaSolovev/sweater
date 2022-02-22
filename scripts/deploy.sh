fabcf4360071ee38f97a6bdacdf15b4fec49b793
Dima <soloveid87@mail.ru>
2022-02-22 14:59:14 +1000

    login test, main controller test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,15 +4,16 @@ mvn clean package
 
 echo 'Copy files...'
 
-scp -i ~/.ssh/id_rsa_drucoder \
-    target/sweater-1.0-SNAPSHOT.jar \
-    dru@192.168.0.107:/home/dru/
+scp -i ~/.ssh/id_rsa \
+    target/sweater-0.0.1-SNAPSHOT.jar \
+    soloveid@172.27.104.149:/home/soloveid/
 
 echo 'Restart server...'
 
-ssh -i ~/.ssh/id_rsa_drucoder dru@192.168.0.107 << EOF
+ssh -tt -i ~/.ssh/id_rsa soloveid@172.27.104.149 << EOF
 pgrep java | xargs kill -9
-nohup java -jar sweater-1.0-SNAPSHOT.jar > log.txt &
+nohup java -jar sweater-0.0.1-SNAPSHOT.jar > log.txt &
 EOF
 
 echo 'Bye'
+
